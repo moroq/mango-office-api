df76b338dbdb8a2998ff343684bf45c6f8770316
moroq <a@moro.me>
2018-11-05 19:41:11 +0300

    Update MangoOfficeError.php

--- src/Section/MangoOfficeError.php
+++ src/Section/MangoOfficeError.php
@@ -1,6 +1,6 @@
 <?php
-namespace Sharoff\Mango\Api;
+namespace moroq\Mango\Api;
 
-Class MangoOfficeError extends \Sharoff\Mango\Api\Base\MangoOfficeError {
+Class MangoOfficeError extends \moroq\Mango\Api\Base\MangoOfficeError {
 
 }
